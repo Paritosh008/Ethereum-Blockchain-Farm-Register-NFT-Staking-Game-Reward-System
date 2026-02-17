f046fc96150b1f8986ca9ef9bb1230f2a842402f
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2026-02-17 11:05:32 +0530

    Update Voeux.sol

--- web3-blockchain-master/contracts/Voeux.sol
+++ web3-blockchain-master/contracts/Voeux.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
-    constructor() ERC20("VOEUX TOKEN", "VOEUX") {
+    constructor() ERC20("V TOKEN", "V") {
         _mint(msg.sender, 200000000*10**18);
     }
 }
